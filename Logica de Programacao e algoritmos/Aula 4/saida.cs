f833651b847da78000b67b79ca08cba6b06bf95c
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-02-18 20:39:14 -0300

    Aula 4

---
+++ Logica de Programacao e algoritmos/Aula 4/saida.cs
@@ -0,0 +1,20 @@
+//Tipos de variaveis e saida de dados
+string nome;//Criado a variavel
+char letra = 'C';//Dado do tipo char
+double valor = 10.5;//Dado do tipo double
+int numero = 10;//Dado do tipo inteiro
+bool booleano = false;//Dado do tipo booleano
+nome = "Senai Americana"; //Dado do tipo string 
+Console.WriteLine(nome);//Saida de dados//
+Console.Write(nome);//Com quebra de linha
+Console.Write(nome);//Sem quebra de linha
+System.Console.WriteLine(nome);//Com snipptes
+string pessoa;
+pessoa = "Clodoaldo";
+Console.WriteLine(pessoa);
+const string ESCOLA = "SENAI Americana 1";//Constante
+System.Console.WriteLine(ESCOLA);
+Console.WriteLine("O nome do usuário é: " + pessoa); //Concatenação
+System.Console.WriteLine("C"+"é"+"s"+"a"+"r");
+//Interpolação
+System.Console.WriteLine($"O nome da escola é: {ESCOLA}");//Interpolação
