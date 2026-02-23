c35507045626813d88ac0662ad15683a1feb4aaa
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-02-23 18:56:22 -0300

    Aula 5

--- Logica de Programacao e algoritmos/Aula 5/troco.cs
+++ Logica de Programacao e algoritmos/Aula 5/troco.cs
@@ -1,10 +1,12 @@
 using System.Runtime.CompilerServices;
 using static System.Console;    
 //Declaração de variaveis
-int preco, quantidade, dinheiroRecebido, troco;
-Write("Digite o valor do produto: ");
-int preco = int.Parse(ReadLine());
-Write("Digite a quantidade do produto");
+Write("Digite o preco: ");
+double preco = double.Parse(ReadLine());
+Write("Digite a quantidade do produto:");
 int quantidade = int.Parse(ReadLine());
 Write("Digite o dinheiro recebido: ");
+double dinheiro = double.Parse(ReadLine());
+double troco = dinheiro - (preco * quantidade);
+Console.WriteLine($"Troco: {troco}");
 
