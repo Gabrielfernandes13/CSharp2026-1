a50659781d5425a281e6568973ed94cc65a7668a
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-03-11 21:50:11 -0300

    Aula 12

--- Logica de Programacao e algoritmos/Aula 12/tupla.cs
+++ Logica de Programacao e algoritmos/Aula 12/tupla.cs
@@ -1,8 +1,11 @@
 //Tupla
-
+using static System.Console; 
 var clodoaldo = (34, 34, "Heitor", "SENAI", 'B');
 System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item1}");
 System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item2}");
 System.Console.WriteLine($"Nome do filho do clodo: {clodoaldo.Item3}");
 clodoaldo.Item1 = 54; 
 System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item1}");
+(int, int, int) numeros = (1, 2, 5);
+
+System.Console.WriteLine($"Números: {numeros.Item1}, {numeros.Item2}, {numeros.Item3}");
