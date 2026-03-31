ffb36ede5cc5c72890dd0cd7f264e99705314c87
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-03-30 21:53:34 -0300

    ContaBancaria

---
+++ POO/Aula 07/BancoMaster/BancoMaster/ContaParaEmpresa.cs
@@ -0,0 +1,6 @@
+﻿namespace BancoMaster
+{
+    internal class ContaParaEmpresa
+    {
+    }
+}
---
+++ POO/Aula 07/BancoMaster/BancoMaster/ContaPoupanca.cs
@@ -0,0 +1,15 @@
+﻿namespace BancoMaster
+{
+    internal class ContaPoupanca : Conta
+
+        //Campos
+        private double taxaJuros;
+
+	//Propriedades
+	public double myVar;
+
+	public double MyProperty
+	{
+		get { return myVar; }
+		set { myVar = value; }
+	}
