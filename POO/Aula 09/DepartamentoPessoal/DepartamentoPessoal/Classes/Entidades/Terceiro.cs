018adfdf3c3a8855f3f202d2fdba7db77fa83f82
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-04-06 21:55:44 -0300

    Aula 09

---
+++ POO/Aula 09/DepartamentoPessoal/DepartamentoPessoal/Classes/Entidades/Terceiro.cs
@@ -0,0 +1,28 @@
+﻿namespace DepartamentoPessoal.Classes.Entidades
+{
+    internal class Terceiro : Funcionario
+    {
+		//Campo
+		private double despesa;
+
+
+        //Propriedade
+        protected double DespesaAdicional
+		{
+			get { return despesa; }
+			set { despesa = value; }
+		}
+
+        //Construtor
+        public Terceiro(string nomeColaborador, int jornadarabalho, double valorHora, double despesa) : base(nomeColaborador, jornadarabalho, valorHora)
+        {
+            DespesaAdicional = despesa;
+        }
+
+        //Método
+        public override double Pagamento()
+        {
+            return base.Pagamento() + (DespesaAdicional * 0.2);
+        }
+	}
+}
