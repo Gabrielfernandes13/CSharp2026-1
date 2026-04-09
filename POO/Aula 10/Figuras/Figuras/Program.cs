8f39441a9eda858081b0119ec58093600c37e919
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-04-08 21:53:11 -0300

    Aula 10

--- POO/Aula 10/Figuras/Figuras/Program.cs
+++ POO/Aula 10/Figuras/Figuras/Program.cs
@@ -1,11 +1,25 @@
 ﻿using Figuras.Classes.Entidades;
 using Figuras.Classes.Enumeracoes;
+using static System.Console;
 
-//Forma geo1 = new Forma (Cor.Vermelho);
-Forma geo2 = new Retangulo(Cor.Azul, 10, 10);
-Forma geo3 = new Circulo(Cor.Rosa, 5);
+List<Forma> formas = new();
 
-Console.WriteLine ($"\nGeo 2 - Tipo Forma -> Retangulo : Area = {geo2.Area()}" +
-                  $"\nGeo 3 - Tipo Forma -> Círculo : Area = {geo3.Area()}");
+Write ("Entre com a quantidade de formas: ");
+int qtd = int.Parse(ReadLine());
 
-Console.ReadKey();
+for (int i = 0; i < qtd; i++)
+{
+    WriteLine("Dados do objeto geometrico nº {i+1}:");
+    Write($"Retângulo ou Círculo (r/c): ");
+    char escolha = char.Parse(ReadLine().ToLower());
+    if (escolha == 'r')
+    {
+        WriteLine("Qual é a Cor do objeto?" +
+            "\n\t1 - Vermelho" +
+            "\n\t2 - Azul" +
+            "n\t3 - Amarelo" +
+            "\n\t4 - Rosa");
+        int cor = int.Parse(ReadLine());
+
+    }
+}
