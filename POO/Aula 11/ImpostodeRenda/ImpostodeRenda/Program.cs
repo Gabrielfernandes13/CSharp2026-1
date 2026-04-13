3fa1305934e57f25b5ad9ba9de0946ec1b0a24b3
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-04-13 17:35:48 -0300

    Imposto de Renda

--- POO/Aula 11/ImpostodeRenda/ImpostodeRenda/Program.cs
+++ POO/Aula 11/ImpostodeRenda/ImpostodeRenda/Program.cs
@@ -1,28 +1,57 @@
 ﻿using static System.Console;
-using ImpostodeRenda.Entidades;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using ImpostoDeRenda.Entidades;
 
-namespace ImpostodeRenda
+namespace ImpostoDeRenda
 {
-class Program
+    class Program
     {
-static void Main(string[] args)
-    {
-    List<Contribuintes> contribuintes = new List<Contribuintes>();
+        static void Main(string[] args)
+        {
+            List<Contribuintes> lista = new List<Contribuintes>();
+
             Console.Write("Digite o número de contribuintes: ");
             int n = int.Parse(Console.ReadLine());
+
             for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine($"\nContribuinte #{i}");
+                Console.WriteLine($"\nContribuinte #{i}:");
                 Console.Write("Pessoa física ou jurídica (f/j)? ");
                 char tipo = char.Parse(Console.ReadLine());
+
                 Console.Write("Nome: ");
                 string nome = Console.ReadLine();
-                Console.Write("Renda anual: ");
-                double rendaAnual = double.Parse(Console.ReadLine());
 
-                if ( (tipo=='f')
+                Console.Write("Renda anual: ");
+                double renda = double.Parse(Console.ReadLine());
+
+                if (tipo == 'f')
                 {
-                    
+                    Console.Write("Gastos com saúde: ");
+                    double gastos = double.Parse(Console.ReadLine());
+
+                    lista.Add(new PessoaFisica(nome, renda, gastos));
+                }
+                else
+                {
+                    Console.Write("Número de funcionários: ");
+                    int funcionarios = int.Parse(Console.ReadLine());
+
+                    lista.Add(new PessoaJuridica(nome, renda, funcionarios));
                 }
             }
+
+            Console.WriteLine("\nIMPOSTOS PAGOS:");
+            double total = 0;
+
+            foreach (Contribuintes c in lista)
+            {
+                double imposto = c.Imposto();
+                Console.WriteLine($"{c.Nome}: R$ {imposto:F2}");
+                total += imposto;
+            }
+
+            Console.WriteLine($"\nTOTAL DE IMPOSTO: R$ {total:F2}");
+        }
+    }
+}
