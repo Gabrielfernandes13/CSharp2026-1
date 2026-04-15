215906313518e20d3ae9b10595934d63d1cbd818
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-04-15 18:38:49 -0300

    ZAbstracao

--- POO/Aula 11/ImpostodeRenda/ImpostodeRenda/obj/Debug/net10.0/ImpostodeRenda.AssemblyInfo.cs
+++ POO/Aula 11/ImpostodeRenda/ImpostodeRenda/obj/Debug/net10.0/ImpostodeRenda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImpostodeRenda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9fe126fb8ef26a59124c0877694d71279164e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc055ce7574b6123a5cbb2f9c1a4ed27f9e61005")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImpostodeRenda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImpostodeRenda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
