9ff831704186a6d83fc48ea3a776a0222a1d9116
Gabrielfernandes13 <gabriel.konvict@gmail.com>
2026-04-24 20:44:13 -0300

    Aula 03 SQL

--- Banco de Dados/Aula 02/Estudante/Estudante/Program.cs
+++ Banco de Dados/Aula 02/Estudante/Estudante/Program.cs
@@ -1,12 +1,25 @@
-﻿using Estudante.Classes.Dados;
+﻿﻿using Estudante.Classes.Dados;
 using Estudante.Classes.Entidades;
-using Microsoft.EntityFrameworkCore;
+
 
 using var context = new AlunoContext();
 
-Aluno pessoa1 = new Aluno("Cesar", 123456, "Python");
+context.Database.EnsureCreated();
+//Create - Adicionar dados ao Banco de dados
+Aluno pessoa1 = new Aluno("Cesar", 123456, "Comandos");
 context.Alunos.Add(pessoa1);
 context.SaveChanges();
 
+//Read - Exibir o dado
 var pessoas = context.Alunos.FirstOrDefault(c => c.Id == 1);
 pessoas.ExibirDados();
+
+//Update
+pessoas = context.Alunos.FirstOrDefault(c => c.Id == 6);
+pessoas.Curso = "Micro";
+context.SaveChanges();
+
+//Delete
+pessoas = context.Alunos.FirstOrDefault(c => c.Id == 3);
+context.Remove(pessoas);
+context.SaveChanges();
