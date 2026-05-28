cf6707709e48c9a8394c0293dc2a701ee76dd2c1
nunesflavio <gabriel.konvict@gmail.com>
2026-05-27 21:51:05 -0300

    Inicio interação banco de dados com paginas web

--- Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/Dados/AlunoContext.cs
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/Dados/AlunoContext.cs
@@ -3,13 +3,32 @@ using ProjetoWeb01.Classes.Entidades;
 
 namespace ProjetoWeb01.Dados
 {
-    public class AlunoContext:DbContext
+    public class AlunoContext : DbContext
     {
         public DbSet<Aluno> Alunos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer();
+            optionsBuilder.UseSqlServer(
+                @"Server = ECFP507D1319371\SQLEXPRESS02;Database=Aluno;Trusted_Connection = True;TrustServerCertificate = True;"
+                );
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>(
+                entity =>
+                {
+                    entity.HasKey(e => e.Id);
+                    entity.Property(e => e.Nome).IsRequired(); //Nome
+                    entity.Property(e => e.Email); //Email
+                    entity.Property(e => e.Senha); //Senha
+                    entity.Property(e => e.CursoID).IsRequired(); //Curso
+                    entity.Property(e => e.RA).IsRequired(); //RA
+                    entity.Property(e => e.StatusAction).IsRequired(); //StatusAction
+                    entity.Property(e => e.StatusWIFI).IsRequired();//StatusWIFI
+
+                }
+                );
         }
     }
 }
